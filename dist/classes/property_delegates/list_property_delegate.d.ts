97b111194548ef29eb91172c7f4d3cb01fdeeb23
Coms <125128463+ComsIndeed@users.noreply.github.com>
2025-12-13 07:54:01 +0800

    chore: move files to packages/llm-json-stream for monorepo merge

--- dist/classes/property_delegates/list_property_delegate.d.ts
+++
@@ -1,7 +0,0 @@
-/**
- * @deprecated Use array_property_delegate.ts instead
- * This file is kept for backward compatibility only
- */
-export { ArrayPropertyDelegate as ListPropertyDelegate } from "./array_property_delegate.js";
-export { ArrayPropertyStreamController as ListPropertyStreamController } from "../property_stream_controller.js";
-//# sourceMappingURL=list_property_delegate.d.ts.map
--- dist/classes/property_delegates/map_property_delegate.js
+++
@@ -1,7 +0,0 @@
-/**
- * @deprecated Use object_property_delegate.ts instead
- * This file is kept for backward compatibility only
- */
-export { ObjectPropertyDelegate as MapPropertyDelegate } from "./object_property_delegate.js";
-export { ObjectPropertyStreamController as MapPropertyStreamController } from "../property_stream_controller.js";
-//# sourceMappingURL=map_property_delegate.js.map
